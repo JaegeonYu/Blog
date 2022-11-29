a2b26a00d1cccd4fdb3a595b3ebe3643a264e883
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-29 14:33:09 +0900

    docs: 리드미 작성

---
+++ src/main/java/com/jacklog/jacklog/JacklogApplication.java
@@ -0,0 +1,13 @@
+package com.jacklog.jacklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacklogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacklog/jacklog/JacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacklog.jacklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
