f72d8d5839fa36389db776953abe3df4c926c9d0
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-29 14:38:06 +0900

    feat: create hello world controller and test

---
+++ src/test/java/com/jacklog/jacklog/controller/PostControllerTest.java
@@ -0,0 +1,27 @@
+package com.jacklog.jacklog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청시 hello world 출력")
+    public void test() throws Exception{
+        mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello world"));
+    }
+}
