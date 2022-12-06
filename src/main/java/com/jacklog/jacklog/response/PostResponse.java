621be42436e38ca99ef6590286990536826e0fa0
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-06 15:39:52 +0900

    feat: PostResponse 클래스 생성

---
+++ src/main/java/com/jacklog/jacklog/response/PostResponse.java
@@ -0,0 +1,18 @@
+package com.jacklog.jacklog.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+
+public class PostResponse {
+    private Long id;
+    private String title;
+    private String content;
+    @Builder
+    public PostResponse(Long id, String title, String content) {
+        this.id = id;
+        this.title = title.substring(0, Math.min(title.length(), 10));
+        this.content = content;
+    }
+}
