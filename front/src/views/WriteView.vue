dfe1d55964e17b887dcf6f8d9948d4bc2aef9c8b
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-17 15:22:44 +0900

    feat: modify vite.config.ts server proxy setting to solve CORS at Front

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -5,7 +5,7 @@ import axios from "axios";
 const title = ref("")
 const content = ref("")
 const write =()=>{
-  axios.post("http://localhost:8080/posts", {
+  axios.post("/api/posts", {
     title: title.value,
     content: content.value
   })
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -12,4 +12,13 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+
+  server:{
+    proxy:{
+      "/api/posts":{
+        target: "http://localhost:8080",
+        rewrite: (path)=>path.replace(/^\/api/, "")
+      }
+    }
+  }
 });
