a00b84e12963c923d7cdc70eff82fa970e409284
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-12-17 16:17:32 +0900

    feat: cretae Post ReadeView

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import axios from "axios";
 import {ref} from "vue";
+import {useRouter} from "vue-router";
 
+const router = useRouter();
 const posts = ref([]);
+
 axios.get("/api/posts?page=1&size=5")
     .then(response => {
       response.data.forEach((r: any) => {
@@ -15,7 +18,9 @@ axios.get("/api/posts?page=1&size=5")
   <ul>
     <li v-for="post in posts" :key="post.id">
       <div>
-        {{ post.title }}
+        <router-link :to="{name: 'read', params:{postId: post.id}}">
+            {{ post.title }}
+        </router-link>
       </div>
       <div>
         {{ post.content }}
